dcff3c99382ca74610f6adea27003d62a5609fd5
SantiMerino <santimerinoh12@gmail.com>
2025-08-25 13:54:38 -0600

    passkey example

---
+++ actividad4.py
@@ -0,0 +1,8 @@
+clave = "FP@2025"
+userinput = input("Ingrese la clave para continuar: ")
+if userinput == clave:
+    print("Clave correcta. Acceso concedido.")
+else:
+    print("Clave incorrecta. Acceso denegado.")
+
+# Ejercicio Opcional
