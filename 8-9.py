a2142b66f2ee5d21effb8daf604f68eb6aaf21e1
SantiMerino <santimerinoh12@gmail.com>
2025-09-08 13:38:57 -0600

    while loop

--- 8-9.py
+++ 8-9.py
@@ -1,13 +1,12 @@
 numero  = input('Ingresa un número entero mayor a 100: ')
-numero = list(numero) # Cifra separada en una lista/matriz 
-numero = map(int, numero) # Puntero de el valor en la memoria
-numero = list(map(int, numero)) # Cifras separadas 
-
-# suma = 0
-# for i in numero:
-#     suma = suma + i
-# Otra forma de hacerlo más simple
-suma = sum(numero) # Función de suma que aparentemente no le importa si es un array
-
-print(numero)
-print(suma) 
+    # suma = 0
+    # for i in numero:
+    #     suma = suma + i
+    # Otra forma de hacerlo más simple
+while len(numero) > 1: # La condición es 1 porque si len es igual a 1 ya no es divisible 
+    numero = list(numero) # Cifra separada en una lista/matriz 
+    numero = map(int, numero) # Puntero de el valor en la memoria
+    numero = list(map(int, numero)) # Cifras separadas 
+    suma = sum(numero) # Función de suma que aparentemente no le importa si es un array
+    print(suma) 
+    numero = str(suma) # Convertimos la suma a cadena y se repite hasta cumplir la condición
