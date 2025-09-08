8b3e3eba21a8a68ca501b527a9bf4928070a8b09
SantiMerino <santimerinoh12@gmail.com>
2025-09-08 13:53:16 -0600

    switch case temu

--- 8-9.py
+++ 8-9.py
@@ -10,3 +10,22 @@ while len(numero) > 1: # La condición es 1 porque si len es igual a 1 ya no es 
     suma = sum(numero) # Función de suma que aparentemente no le importa si es un array
     print(suma) 
     numero = str(suma) # Convertimos la suma a cadena y se repite hasta cumplir la condición
+
+active = True
+while active:
+    print('1. Opción 1')
+    print('2. Opción 2')
+    print('3. Opción 3')
+    print('4. Salir')
+    opcion = int(input('Ingrese opción: '))
+    def switch_temu(opcion):
+        match opcion:
+            case 1:
+                print('Eligio la opción 1')
+            case 2: 
+                print('Eleigio la opción 2')
+            case 3: 
+                print('Eleigio la opción 2')
+            case 4: 
+                print('El usuario eligio salir')
+                active = False
