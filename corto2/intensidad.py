0a7920ed526d5d48636c79998146a4a3d9d05d7a
SantiMerino <santimerinoh12@gmail.com>
2025-10-11 10:16:07 -0600

    validation and explanation

--- corto2/intensidad.py
+++ corto2/intensidad.py
@@ -10,12 +10,22 @@ for i in range(10):
     tiempo_intensidad.append((i+1, intensidades[i]))
 print(f"Minuto e intensidad registrada: {tiempo_intensidad}")
 # Valor máximo y tiempo de intensidad
-max_intensidad = max(intensidades)
+max_intensidad = []
+for i in range(10):
+    valor = max(intensidades)
+    if intensidades[i] == valor:
+        max_intensidad.append((i+1, intensidades[i]))
 print('---------------------------------------------------------')
-print(f"Máximo: {max_intensidad} (minuto {intensidades.index(max_intensidad)+1})")
-min_intensidad = min(intensidades)
+print(f"Máximo: {valor} (minuto {intensidades.index(valor)+1})")
+print(f"Minuto e intensidad máxima registrada: {max_intensidad}")
+min_intensidad = []
+for i in range(10):
+    valor = min(intensidades)
+    if intensidades[i] == valor:
+        min_intensidad.append((i+1, intensidades[i]))
 print('---------------------------------------------------------')
-print(f"Minímo: {min_intensidad} (minuto {intensidades.index(min_intensidad)+1})")
+print(f"Minímo: {valor} (minuto {intensidades.index(valor)+1})")
+print(f"Minuto e intensidad mínima registrada: {min_intensidad}")
 # Promedio de intensidad
 promedio_intensidad = sum(intensidades) / len(intensidades)
 print('---------------------------------------------------------')
@@ -43,9 +53,15 @@ else:
 # x' = (x - min) / (max - min)
 intensidades_norm = [0]*10
 for i in range(10):
-    intensidades_norm[i] = (intensidades[i] - min(intensidades)) / (max(intensidades) - min(intensidades))
+    if max(intensidades) - min(intensidades) == 0:
+        intensidades_norm[i] = 0
+    else:
+        intensidades_norm[i] = (intensidades[i] - min(intensidades)) / (max(intensidades) - min(intensidades))
 print('---------------------------------------------------------')
 #Comparación de listas con comprensión de listas 
 print(f"Intensidades oringiales: {intensidades}")
 print(f"Intensidades normalizadas: {[round(x, 2) for x in intensidades_norm]}")
 
+# Agregue dos funcionalidades al programa:
+# 1. Existe la posibilidad que la intensidad no cambie por lo que la normalización no se podría realizar. En ese caso, el programa debe indicar que la normalización es 0
+# 2. Existe la posibilidad de tener el valor máximo es distintos minutos durante la prueba de intensidad, por lo que el programa debe indicar todos los minutos en los que se obtuvo el valor máximo.
