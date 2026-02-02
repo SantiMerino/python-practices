371b40e299c96d8c229a5f11f7bca52cb772d1ab
SantiMerino <santimerinoh12@gmail.com>
2026-02-02 12:28:52 -0600

    java classes

---
+++ java-practices/class02-02/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main{
+    public static void main(String[]  args){
+        System.out.println("Hola Mundo");
+//    Creando objetos de la clase Estudainte
+        Estudiante estudiante1 = new Estudiante();
+    }
+}
+
