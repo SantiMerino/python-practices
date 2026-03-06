64d09e61fb603bf2472067314c7ba53a7609b741
SantiMerino <santimerinoh12@gmail.com>
2026-03-05 18:32:52 -0600

    delete java

--- java-practices/class02-02/src/Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main{
-    public static void main(String[]  args){
-        System.out.println("Hola Mundo");
-//    Creando objetos de la clase Estudainte
-        Estudiante estudiante1 = new Estudiante();
-    }
-}
-
